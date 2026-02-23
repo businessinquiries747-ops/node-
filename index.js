f5d87ac9087c59ebd4282605a82a40612b869134
businessinquiries747-ops <business.inquiries747@gmail.com>
2026-02-23 02:58:41 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.end(`
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>Test</title>
+      </head>
+      <body>
+        <h1>Otvorite konzolu (F12)</h1>
+        <script>
+          console.log("Zdravo iz browser konzole!");
+        </script>
+      </body>
+    </html>
+  `);
+});
+
+server.listen(3000, () => {
+  console.log('Server pokrenut na http://localhost:3000');
+});
