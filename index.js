adc553186731ff739966e4bd03b721e227e9b3c6
businessinquiries747-ops <business.inquiries747@gmail.com>
2026-02-23 03:10:21 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,12 @@
-const http = require('http');
+const http = require("http");
+
+const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end(`
-    <!DOCTYPE html>
-    <html>
-      <head>
-        <title>Test</title>
-      </head>
-      <body>
-        <h1>Otvorite konzolu (F12)</h1>
-        <script>
-          console.log("Zdravo iz browser konzole!");
-        </script>
-      </body>
-    </html>
-  `);
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Radi cisti Node.js na Renderu 🚀");
 });
 
-server.listen(3000, () => {
-  console.log('Server pokrenut na https://node-3dti.onrender.com/');
-
+server.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
-
-
